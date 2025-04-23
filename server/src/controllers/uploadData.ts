73da2aca4c1cb30380d943bc153558436d64e5e2
k4nkan <kanta.y.0126@gmail.com>
2025-04-23 09:28:23 +0900

    [add] upload image url to table

--- server/src/controllers/uploadData.ts
+++ server/src/controllers/uploadData.ts
@@ -16,17 +16,43 @@ export const uploadData = async (
   }
 
   try {
+    // webp 変換
     const webpBuffer = await sharp(file.buffer).webp().toBuffer();
     const fileName = `${uuid()}.webp`;
+
+    // storage にアップロード
     const { data: storageData, error: storageError } = await supabase.storage
       .from("nagoya-photos")
       .upload(fileName, webpBuffer, {
         contentType: "image/webp",
       });
+
     if (storageError) {
       res.status(500).json({ error: storageError.message });
       return;
     }
+
+    // storage から url を取得
+    const { data: publicURLData } = supabase.storage
+      .from("nagoya-photos")
+      .getPublicUrl(storageData.path);
+
+    const imageURL = publicURLData?.publicUrl;
+    if (!imageURL) {
+      res.status(500).json({ error: "failed to get public URL" });
+      return;
+    }
+
+    // 習得した url を table に保存
+    const { error: insertError } = await supabase
+      .from("nagoya-datas")
+      .insert([{ "image-url": imageURL, "photo-date": day }]);
+
+    if (insertError) {
+      res.status(500).json({ error: insertError.message });
+      return;
+    }
+    res.status(200).json({ message: "done!" });
   } catch (err: any) {
     res.status(500).json({ error: err.message || "unknown error" });
     return;
