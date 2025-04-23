b3d8554c829b727f6f4e3923ca16ade8575f8b97
k4nkan <kanta.y.0126@gmail.com>
2025-04-23 09:45:12 +0900

    [fix] getAllData fix

--- server/src/controllers/getAllData.ts
+++ server/src/controllers/getAllData.ts
@@ -2,13 +2,30 @@ import { supabase } from "../supabase/client";
 import { Request, Response } from "express";
 
 export const getAllData = async (
-  _req: Request,
+  req: Request,
   res: Response
 ): Promise<void> => {
-  const { data, error } = await supabase.from("nagoya-datas").select("*");
+  const day = req.query.day as string;
+
+  if (!day) {
+    res.status(400).json({ error: "can't found day qury" });
+    return;
+  }
+
+  // storage から データを取得
+  const { data, error } = await supabase
+    .from("nagoya-datas")
+    .select("*")
+    .eq("photo-date", day);
+
   if (error) {
     res.status(500).json({ error: error.message });
     return;
   }
+
+  if (!data || data.length === 0) {
+    res.json({ massage: "none" });
+  }
+
   res.json(data);
 };
