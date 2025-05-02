c7c8cacd80f46e881e58e2a53bf3f2e5517bc300
k4nkan <kanta.y.0126@gmail.com>
2025-05-03 01:11:09 +0900

    #17 画像読み込み時のローディング実装

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,10 @@
 import { fetchImgData } from "./api/fetchImgData.js";
 import { displayImages } from "./ui/displayImg.js";
+import loading from "./ui/loading.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  loading.showLoading();
   await fetchImgData();
   displayImages();
+  loading.hideLoading();
 });
