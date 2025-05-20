9caa74b7c2d1952310f30fbd2952db26d36735b3
k4nkan <kanta.y.0126@gmail.com>
2025-05-20 20:06:20 +0900

    #68 fetchの切り替えエラーの修正

--- public/scripts/data/urlData.js
+++ public/scripts/data/urlData.js
@@ -1,5 +1,5 @@
 const isLocalhost = window.location.hostname === "localhost";
 
 export const API_BASE_URL = isLocalhost
-  ? "https://sun-a-nagoya-memories-backend.onrender.com"
-  : "http://localhost:3000";
+  ? "http://localhost:3000"
+  : "https://sun-a-nagoya-memories-backend.onrender.com";
